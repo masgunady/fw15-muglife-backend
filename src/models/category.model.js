97b224527182e28ff2b0e593600ca36143de69cd
putubagus13 <putu031216@gmail.com>
2023-06-16 22:07:27 +0700

    fix: category

---
+++ src/models/category.model.js
@@ -0,0 +1,26 @@
+const db = require("../helpers/db.helper")
+
+exports.findAll = async function(page, limit, search, sort, sortBy){
+    page = parseInt(page) || 1
+    limit = parseInt(limit) || 5
+    search = search || ""
+    sort = sort || "id"
+    sortBy = sortBy || "ASC"
+    const offset = (page -1)* limit
+    const query= `
+    SELECT * FROM "categories" WHERE "name" LIKE $3 ORDER BY ${sort} ${sortBy} LIMIT $1 OFFSET $2`
+
+    const values = [limit, offset, `%${search}%`]
+    const {rows} = await db.query(query,values)
+    return rows
+}
+
+exports.insert = async function (data) {
+    const query = `
+    INSERT INTO "categories" ("name")
+    VALUES ($1) RETURNING *
+    `
+    const values = [data.name]
+    const { rows } = await db.query(query, values)
+    return rows[0]
+}
