550d6cb4d5d0798ef8305109bfec4f4cc2bb5dd9
putubagus13 <putu031216@gmail.com>
2023-06-17 06:33:22 +0700

    add: uploadMidleware

--- src/routers/products.router.js
+++ src/routers/products.router.js
@@ -1,12 +1,12 @@
 const products = require("express").Router()
 // const validate = require("../middlewares/validator.middleware")
-// const uploadMiddleware = require("../middlewares/upload.middlewares")
+const {uploadMiddleware} = require("../middlewares/upload.middleware")
 // const authMiddleware = require("../middlewares/auth.middleware")
 const productsController = require("../controllers/products.controller")
 
 products.get("/", productsController.getAllProducts)
 products.get("/:id", productsController.getOneProductsNonUser)
-products.post("/admin/", productsController.createProducts)
+products.post("/admin/",  uploadMiddleware("picture"), productsController.createProducts)
 // products.patch("/", uploadMiddleware("picture"), productsController.updateProfile)
 
 module.exports = products
