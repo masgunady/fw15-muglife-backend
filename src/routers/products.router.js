2feca767a855582f72e6215a051525460cea459c
putubagus13 <putu031216@gmail.com>
2023-06-18 21:09:14 +0700

    add: fine one product

--- src/routers/products.router.js
+++ src/routers/products.router.js
@@ -5,7 +5,7 @@ const {uploadMiddleware} = require("../middlewares/upload.middleware")
 const productsController = require("../controllers/products.controller")
 
 products.get("/", productsController.getAllProducts)
-products.get("/:id", productsController.getOneProductsNonUser)
+products.get("/:id", productsController.getOne)
 products.post("/admin/",  uploadMiddleware("picture"), productsController.createProducts)
 products.patch("/admin/:id", uploadMiddleware("picture"), productsController.updateProduct)
 
